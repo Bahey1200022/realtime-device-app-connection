b94bba7d2296d5ea7e8a26374090fdc9623a61e3
Bahey <bahy2002@gmail.com>
2024-05-03 12:09:48 +0300

    setup

--- device/backend.py
+++ device/backend.py
@@ -1,5 +1,8 @@
 import socket
-
+from signal import generate_ecg_signal
+import random
+import json
+#### here we are emulating the medical device
 HOST= '0.0.0.0'
 
 PORT=2001
@@ -10,16 +13,16 @@ with socket.socket(socket.AF_INET,socket.SOCK_STREAM) as app_socket:
 
     print((HOST,PORT))
 
-#to run :python backend.py  cd device
+#to run : python backend.py  cd device
     conn,add=app_socket.accept()
     with conn:
         print('ip',add)  
-
-        data=conn.recv(1024) 
-
-        print('data:',data.decode())
-        response='socket-connection'+data.decode()
-        conn.sendall(response.encode())
+        x, y = generate_ecg_signal()
+        id = random.randint(1, 100)
+        
+        response = {'x': x.tolist(), 'y': y.tolist(), 'id': id}
+        json_response = json.dumps(response)
+        conn.sendall(json_response.encode())
         
 
 
